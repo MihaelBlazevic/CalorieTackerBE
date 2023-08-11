f30d4a9ca028b531a51cb53719f9d72ac9f4c35d
Ernest Dudaković <laximas1@gmail.com>
2023-08-11 16:20:00 +0200

    napravljen login page i route za home

--- src/router/router.js
+++ src/router/router.js
@@ -5,6 +5,25 @@ const User = require('../templates/user');
 router.post('/register', function(req, res, next) {
   const { email, password } = req.body;
 
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      const user = await User.findOne({ email, password }).exec();
+  
+      if (!user) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+      }
+      
+      // If the user exists and the credentials are correct, return the user details
+      return res.status(200).json({ message: `SUCCESSFULLY LOGGED IN, ${user.username}!!!`})
+      ret
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+
   // Validate that both email and password fields are provided
   if (!email || !password) {
     return res.status(400).json({ message: 'Email and password are required' });
