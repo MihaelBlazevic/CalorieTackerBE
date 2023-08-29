1bdd6287c8d06bb5ad7ae803fca4852375380580
unknown <mihael75.blazevic@gmail.com>
2023-08-29 14:54:04 +0200

    popravljen backend login function do radi

--- src/router/router.js
+++ src/router/router.js
@@ -3,27 +3,28 @@ const router = express.Router();
 const User = require('../templates/user');
 const ingredients = require('../data/ingredients');
 
+// Login Route
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email, password }).exec();
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    // If the user exists and the credentials are correct, return the user details
+    return res.status(200).json({ message: `SUCCESSFULLY LOGGED IN, ${user.name}!!!` });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Register Route
 router.post('/register', function (req, res, next) {
   const { name, surname, email, password } = req.body;
-
-  router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-
-    try {
-      const user = await User.findOne({ email, password }).exec();
-
-      if (!user) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-      }
-
-      // If the user exists and the credentials are correct, return the user details
-      return res.status(200).json({ message: `SUCCESSFULLY LOGGED IN, ${user.username}!!!` })
-      ret
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: 'Internal server error' });
-    }
-  });
 
   // Validate that both email and password fields are provided
   if (!email || !password) {
@@ -32,7 +33,7 @@ router.post('/register', function (req, res, next) {
 
   User.create({ name, surname, email, password })
     .then(function (user) {
-      console.log("User Created!"); // Move the console.log here
+      console.log("User Created!");
       res.send(user);
     })
     .catch(next);
